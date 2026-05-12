c3f34ba85daf2797df661697b2c1dae25636bc29
sea.yb <sea.yb@ccn.com.sg>
2026-05-12 10:06:29 +0800

    chore: fix formatting in preload

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -10,7 +10,7 @@ contextBridge.exposeInMainWorld('api', {
   createPty:  (p: PtyCreatePayload): Promise<PtyCreatedPayload> => ipcRenderer.invoke(IPC.PTY_CREATE, p),
   sendInput:  (p: PtyInputPayload)  => ipcRenderer.send(IPC.PTY_INPUT,   p),
   resizePty:  (p: PtyResizePayload) => ipcRenderer.send(IPC.PTY_RESIZE,  p),
-  destroyPty: (p: PtyDestroyPayload)=> ipcRenderer.send(IPC.PTY_DESTROY, p),
+  destroyPty: (p: PtyDestroyPayload) => ipcRenderer.send(IPC.PTY_DESTROY, p),
 
   onData: (cb: (p: PtyDataPayload) => void) => {
     const fn = (_: Electron.IpcRendererEvent, p: PtyDataPayload) => cb(p);
